aab1cb2ec2fdac0a28875b5b3060ae6acee7d8bb
milind31 <milind.rv@gmail.com>
2021-05-26 23:43:38 -0700

    lighting + scroll arrows + fonts

--- PersonalWebsite/main.js
+++ PersonalWebsite/main.js
@@ -21,33 +21,38 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 // create torus shape
-const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
-const material = new THREE.MeshBasicMaterial( {
-	color: 0x7ae7ff,
+const torusGeometry = new THREE.TorusKnotGeometry( 10, 2, 100, 16 );
+const torusMaterial = new THREE.MeshStandardMaterial( {
+	color: 0x3bbaf5,
   wireframe: true,
 });
+const torus = new THREE.Mesh( torusGeometry, torusMaterial );
+scene.add(torus);
 
-const shape = new THREE.Mesh( geometry, material );
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
 
-scene.add(shape);
+const directional1 = new THREE.DirectionalLight( 0xffffff, 0.6 );
+directional1.position.set(-20,40,0);
+scene.add(directional1);
 
-// add lighting
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight)
+const directional2 = new THREE.DirectionalLight( 0xffffff, 0.6 );
+directional2.position.set(10,-40,0);
+scene.add(directional2);
 
 // add stars
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.1,24,24);
+  const geometry = new THREE.SphereGeometry(0.2,24,24);
   const material = new THREE.MeshStandardMaterial({color: 0xffffff, wireframe: true })
   const star = new THREE.Mesh(geometry, material);
 
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(275));
 
   star.position.set(x,y,z);
   scene.add(star);
 }
 
-Array(200).fill().forEach(addStar);
+Array(1000).fill().forEach(addStar);
 
 // background
 const texture = new THREE.TextureLoader().load('tron2.jpg');
@@ -57,12 +62,13 @@ scene.background = texture;
 function animate() {
   requestAnimationFrame( animate );
 
+  torus.rotation.x += 0.002;
+  torus.rotation.y += 0.002;
+  torus.rotation.y += 0.001;
 
-  shape.rotation.x += 0.002;
-  shape.rotation.y += 0.002;
-  shape.rotation.y += 0.001;
-
+  renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.render( scene, camera );
+  
 }
 
 // initialize camera position
@@ -74,16 +80,24 @@ camera.position.y = 0;
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  shape.rotation.x += 0.001;
-  shape.rotation.y += 0.001;
-  shape.rotation.z += 0.0005;
+  torus.rotation.x += 0.001;
+  torus.rotation.y += 0.001;
+  torus.rotation.z += 0.0005;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
   camera.position.y = t * -0.0002;
 
+
+
 }
 
 document.body.onscroll = moveCamera;
+
+document.onscroll = function() {
+  if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+      document.getElementById('arrows').style.display='none';
+  }
+}
 
 animate();
